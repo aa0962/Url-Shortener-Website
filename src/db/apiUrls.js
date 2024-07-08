a37ba1bd7c7818f9dd6f3c6986d9dd8b0db6a4c4
aa0962 <aa0962@srmist.edu.in>
2024-07-08 19:29:16 +0530

    init:ApiUrls

---
+++ src/db/apiUrls.js
@@ -0,0 +1,15 @@
+import supabase from "./supabase";
+
+export async function getUrls(user_id)
+{
+    const {data,error}=await supabase
+    .from("urls")
+    .select("*")
+    .eq("user_id",user_id)
+
+    
+    if(error){
+        console.error(error.message)
+        throw new Error(error.message);}
+    return data;
+}
