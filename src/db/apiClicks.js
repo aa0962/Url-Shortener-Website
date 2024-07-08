691b1a44401aff41cab35a0ecd5b10c5b2c05995
aa0962 <aa0962@srmist.edu.in>
2024-07-08 19:33:49 +0530

    init:ApiClicks

---
+++ src/db/apiClicks.js
@@ -0,0 +1,15 @@
+import supabase from "./supabase";
+
+export async function getClicksForUrls(urlIds){
+    const {data,error}=await supabase
+    .from("clicks")
+    .select("*")
+    .in("utl_id",urlIds)
+
+    if(error){
+        console.error(error.message)
+        throw new Error("Unable to load Clicks")
+    }
+
+    return data;
+}
