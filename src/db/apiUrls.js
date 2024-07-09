15ff3cbf591877894b792a4ab94d7c0b8fbc3af0
aa0962 <aa0962@srmist.edu.in>
2024-07-10 01:15:32 +0530

    init:UAParser

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id)
@@ -61,3 +62,42 @@ export async function createUrl({title,longUrl,customUrl,user_id},qrcode)
         throw new Error("Error creating Short URL");}
     return data;
 }
+
+export async function getLongUrl(id)
+{
+    const {data,error}=await supabase
+    .from("urls")
+    .select("id,original_url")
+    .or(`short_url.eq.${id},custom_url.eq.${id}`)
+    .single();
+
+    
+    if(error){
+        console.error(error.message)
+        throw new Error("Error fetching short link");}
+    return data;
+}
+
+const parser=new UAParser();
+
+export const storeClicks=async({id,originalUrl}=>{
+    try{
+        const res=parser.getResult();
+        const device=res.type || "desktop";
+
+        const response=await fetch("https://ipapi.co/json");
+        const {city,country_name:country}=await response.json();
+
+        await supabase.from("clicks").insert({
+            url_id:id,
+            city:city,
+            country:country,
+            device:device,
+        })
+
+        window.location.href=originalUrl;
+    }catch(error){
+        console.error("Error recording click:",error)
+
+    }
+})
