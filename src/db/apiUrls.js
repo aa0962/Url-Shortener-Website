3ff6f4adc585dc9a967d0988b8fd9749c4c02f57
aa0962 <aa0962@srmist.edu.in>
2024-07-10 14:25:32 +0530

    init:Finished

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,98 +1,91 @@
+import supabase, {supabaseUrl} from "./supabase";
 
-import supabase, { supabaseUrl } from "./supabase";
-
-export async function getUrls(user_id)
-{
-    const {data,error}=await supabase
+export async function getUrls(user_id) {
+  let {data, error} = await supabase
     .from("urls")
     .select("*")
-    .eq("user_id",user_id)
+    .eq("user_id", user_id);
 
-    
-    if(error){
-        console.error(error.message)
-        throw new Error(error.message);}
-    return data;
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to load URLs");
+  }
+
+  return data;
 }
 
-export async function deleteUrl(id)
-{
-    const {data,error}=await supabase
+export async function getUrl({id, user_id}) {
+  const {data, error} = await supabase
     .from("urls")
-    .delete()
-    .eq("id",id)
+    .select("*")
+    .eq("id", id)
+    .eq("user_id", user_id)
+    .single();
 
-    
-    if(error){
-        console.error(error.message)
-        throw new Error(error.message);}
-    return data;
+  if (error) {
+    console.error(error);
+    throw new Error("Short Url not found");
+  }
+
+  return data;
 }
 
-export async function createUrl({title,longUrl,customUrl,user_id},qrcode)
-{   const short_url=Math.random().toString(36).substring(2,6)
-    const fileName = `qr-${short_url}`;
-  
-    const {error: storageError} = await supabase.storage
-      .from("qrs")
-      .upload(fileName, qrcode);
-  
-    if (storageError) throw new Error(storageError.message);
-
-    const qr=`${supabaseUrl}/storage/v1/object/public/profile_pic/qrs/${fileName}`
-
-    const {data,error}=await supabase
+export async function getLongUrl(id) {
+  let {data: shortLinkData, error: shortLinkError} = await supabase
     .from("urls")
-    .insert([
-        {
-          title,
-          original_url: longUrl,
-          custom_url: customUrl || null,
-          user_id,
-          short_url,
-          qr
-        }
-
-    ])
-    .select();
-
-    
-    if(error){
-        console.error(error.message)
-        throw new Error("Error creating Short URL");}
-    return data;
-}
-
-export async function getLongUrl(id)
-{
-    const {data,error}=await supabase
-    .from("urls")
-    .select("id,original_url")
+    .select("id, original_url")
     .or(`short_url.eq.${id},custom_url.eq.${id}`)
     .single();
 
-    
-    if(error){
-        console.error(error.message)
-        throw new Error("Error fetching short link");}
-    return data;
+  if (shortLinkError && shortLinkError.code !== "PGRST116") {
+    console.error("Error fetching short link:", shortLinkError);
+    return;
+  }
+
+  return shortLinkData;
 }
 
-export async function getUrl({id,user_id})
-{
-    const {data,error}=await supabase
+export async function createUrl({title, longUrl, customUrl, user_id}, qrcode) {
+  const short_url = Math.random().toString(36).substr(2, 6);
+  const fileName = `qr-${short_url}`;
+
+  const {error: storageError} = await supabase.storage
+    .from("qrs")
+    .upload(fileName, qrcode);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${fileName}`;
+
+  const {data, error} = await supabase
     .from("urls")
-    .select("*")
-    .eq("user_id",user_id)
-    .single()
-    .eq("id",id)
+    .insert([
+      {
+        title,
+        user_id,
+        original_url: longUrl,
+        custom_url: customUrl || null,
+        short_url,
+        qr,
+      },
+    ])
+    .select();
 
-    
+  if (error) {
+    console.error(error);
+    throw new Error("Error creating short URL");
+  }
 
-    
-    if(error){
-        console.error(error.message)
-        throw new Error("Error fetching short link");}
-    return data;
+  return data;
 }
 
+export async function deleteUrl(id) {
+  const {data, error} = await supabase.from("urls").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to delete Url");
+  }
+
+  return data;
+}
